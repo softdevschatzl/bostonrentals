6eabd430ef6719b2404d52eb72f10037fec0fceb
theschatzinator <johnschatzl@yahoo.com>
2024-03-05 00:06:53 -0500

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -77,7 +77,7 @@ export default {
   html {
     height: 100%;
     font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-    background: rgb(69, 69, 69);
+    background: black;
   }
 
   * {
