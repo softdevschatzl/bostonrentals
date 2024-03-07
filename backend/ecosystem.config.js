8e544195b2535776fcb9d0bcff6ee173f80553de
theschatzinator <johnschatzl@yahoo.com>
2024-03-07 13:08:10 -0500

    added for pm2 configuration

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    apps : [{
+      name: "arentals-backend",
+      script: "./index.js", // Path to your backend entry point
+      cwd: "./"             // Working directory set to the backend folder
+    }]
+  }
+  
